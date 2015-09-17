fe2e4857ee1fd5c7e0a8c6a534f3b3b5cd82c257
segmentationfaulter <segmentationfaulter@gmail.com>
2015-09-17 13:05:30 +0500

    do the second challenge

---
+++ 2.js
@@ -0,0 +1,9 @@
+function repeat(operation, num) {
+    'use strict';
+    if (num === 0) {
+      return;
+    }
+    operation();
+    repeat(operation, num-1);
+}
+module.exports = repeat;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # functional-js-workshop
+An attempt to solve the functional javascript workshop, you can find it here
+https://github.com/timoxley/functional-javascript-workshop
